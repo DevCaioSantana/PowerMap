11649f84f0776df9d2812c09527f2181bf4fcf99
DevCaioSantana <caiovitorvs@hotmail.com>
2024-12-09 16:39:49 -0300

    Atualiza configuração de CORS e ajusta .gitignore

--- powermap/src/main/java/com/example/powermap/config/WebConfig.java
+++ powermap/src/main/java/com/example/powermap/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.example.powermap.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,10 +9,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**") // Aplica CORS para todos os endpoints
+                .allowedOrigins(
+                        "http://localhost:4200", // Permite localhost para desenvolvimento
+                        "https://powermap-production-a419.up.railway.app" // Permite o domínio de produção
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permite todos os headers
+                .allowCredentials(true); // Permite envio de cookies ou autenticação
     }
 }
